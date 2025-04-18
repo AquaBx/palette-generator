6485ba50bab45f14869bff5edab84c87b26908d6
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2025-04-18 19:17:04 +0200

    update to tailwind 4 add export code

---
+++ src/components/DemoLayout.svelte
@@ -0,0 +1,214 @@
+<script lang="ts"></script>
+
+<div
+  class="mx-auto mt-16 grid max-w-lg grid-cols-1 items-center gap-y-6 sm:mt-20 sm:gap-y-0 lg:max-w-4xl lg:grid-cols-2"
+>
+  <div
+    class="rounded-3xl rounded-t-3xl bg-color-950 p-8 ring-1 ring-color-900 ring-opacity-10 bg-opacity-60 sm:mx-8 sm:rounded-b-none sm:p-10 lg:mx-0 lg:rounded-bl-3xl lg:rounded-tr-none"
+  >
+    <h3 id="tier-hobby" class="text-base/7 font-semibold text-compl-500">
+      Hobby
+    </h3>
+    <p class="mt-4 flex items-baseline gap-x-2">
+      <span class="text-5xl font-semibold tracking-tight text-color-200"
+        >$29</span
+      >
+      <span class="text-base text-color-300">/month</span>
+    </p>
+    <p class="mt-6 text-base/7 text-color-300">
+      The perfect plan if you&#039;re just getting started with our product.
+    </p>
+    <ul role="list" class="mt-8 space-y-3 text-sm/6 text-color-300 sm:mt-10">
+      <li class="flex gap-x-3">
+        <svg
+          class="h-6 w-5 flex-none text-compl-500"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          aria-hidden="true"
+          data-slot="icon"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+            clip-rule="evenodd"
+          />
+        </svg>
+        25 products
+      </li>
+      <li class="flex gap-x-3">
+        <svg
+          class="h-6 w-5 flex-none text-compl-500"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          aria-hidden="true"
+          data-slot="icon"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+            clip-rule="evenodd"
+          />
+        </svg>
+        Up to 10,000 subscribers
+      </li>
+      <li class="flex gap-x-3">
+        <svg
+          class="h-6 w-5 flex-none text-compl-500"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          aria-hidden="true"
+          data-slot="icon"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+            clip-rule="evenodd"
+          />
+        </svg>
+        Advanced analytics
+      </li>
+      <li class="flex gap-x-3">
+        <svg
+          class="h-6 w-5 flex-none text-compl-500"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          aria-hidden="true"
+          data-slot="icon"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+            clip-rule="evenodd"
+          />
+        </svg>
+        24-hour support response time
+      </li>
+    </ul>
+    <a
+      href="#"
+      aria-describedby="tier-hobby"
+      class="mt-8 block rounded-md px-3 py-2 text-center text-sm font-semibold text-compl-200 ring-1 ring-inset ring-compl-200 hover:text-compl-300 hover:ring-compl-300 sm:mt-10"
+      >Get started today</a
+    >
+  </div>
+  <div
+    class="relative rounded-3xl bg-color-900 p-8 shadow-2xl ring-1 ring-color-900 ring-opacity-10 sm:p-10"
+  >
+    <h3 id="tier-enterprise" class="text-base/7 font-semibold text-color-50">
+      Enterprise
+    </h3>
+    <p class="mt-4 flex items-baseline gap-x-2">
+      <span class="text-5xl font-semibold tracking-tight text-color-50"
+        >$99</span
+      >
+      <span class="text-base text-color-400">/month</span>
+    </p>
+    <p class="mt-6 text-base/7 text-color-100">
+      Dedicated support and infrastructure for your company.
+    </p>
+    <ul role="list" class="mt-8 space-y-3 text-sm/6 text-color-100 sm:mt-10">
+      <li class="flex gap-x-3">
+        <svg
+          class="h-6 w-5 flex-none text-compl-500"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          aria-hidden="true"
+          data-slot="icon"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+            clip-rule="evenodd"
+          />
+        </svg>
+        Unlimited products
+      </li>
+      <li class="flex gap-x-3">
+        <svg
+          class="h-6 w-5 flex-none text-compl-500"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          aria-hidden="true"
+          data-slot="icon"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+            clip-rule="evenodd"
+          />
+        </svg>
+        Unlimited subscribers
+      </li>
+      <li class="flex gap-x-3">
+        <svg
+          class="h-6 w-5 flex-none text-compl-500"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          aria-hidden="true"
+          data-slot="icon"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+            clip-rule="evenodd"
+          />
+        </svg>
+        Advanced analytics
+      </li>
+      <li class="flex gap-x-3">
+        <svg
+          class="h-6 w-5 flex-none text-compl-500"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          aria-hidden="true"
+          data-slot="icon"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+            clip-rule="evenodd"
+          />
+        </svg>
+        Dedicated support representative
+      </li>
+      <li class="flex gap-x-3">
+        <svg
+          class="h-6 w-5 flex-none text-compl-500"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          aria-hidden="true"
+          data-slot="icon"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+            clip-rule="evenodd"
+          />
+        </svg>
+        Marketing automations
+      </li>
+      <li class="flex gap-x-3">
+        <svg
+          class="h-6 w-5 flex-none text-compl-500"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          aria-hidden="true"
+          data-slot="icon"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+            clip-rule="evenodd"
+          />
+        </svg>
+        Custom integrations
+      </li>
+    </ul>
+    <a
+      href="#"
+      aria-describedby="tier-enterprise"
+      class="mt-8 block rounded-md bg-compl-500 px-3.5 py-2.5 text-center text-sm font-semibold text-color-950 shadow-xs hover:bg-compl-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-compl-500 sm:mt-10"
+      >Get started today</a
+    >
+  </div>
+</div>
--- src/components/Input.svelte
+++ src/components/Input.svelte
@@ -1,5 +1,11 @@
-<script>
-    let {value = $bindable(), min, max, type}=$props()
+<script lang="ts">
+  let { value = $bindable(), min, max, type } = $props();
 </script>
 
-<input type={type} min={min} max={max} bind:value={value} class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+<input
+  {type}
+  {min}
+  {max}
+  bind:value
+  class="block w-full p-2 text-compl-400 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+/>
